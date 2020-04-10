5cf882d5802080b9ad5ee2b8323736794215bf09
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-04-10 17:15:53 -0500

    implements main module

--- __main__.py
+++ __main__.py
@@ -1,2 +1,9 @@
+import collatz_conjecture as cc
+
+def main():
+    current, steps = cc.collatz_conjecture(input('Enter a number greater than 1: '))
+    print(f'Result: {current}')
+    print(f'Steps to result: {steps}')
+
 if __name__ == '__main__':
-    print('Hello world')
+    main()
